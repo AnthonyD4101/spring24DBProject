c3f8a1a641c160c54cfc879f5b51ec97e2f9058c
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-03-12 16:58:16 -0500

    added signUp page for user account creation

--- spring24DBProject/frontend/src/pages/signUp.jsx
+++ spring24DBProject/frontend/src/pages/signUp.jsx
@@ -3,5 +3,117 @@ import React from "react";
 import classes from "../components/UI/SignUp.module.css";
 
 export default function SignUp() {
-  return <h1>SignUp Page</h1>;
+  return (
+    <div className="row justify-content-center">
+      <div className="col md-4 mb-4">
+        <div className="card sign-up">
+          <div className="card-body">
+            <h1 className="my-2 text-center" style={{ color: "#2F4858" }}>
+              Create Account
+            </h1>
+            <form>
+              <div className="row mt-5 mb-3">
+                <div className="col">
+                  <label htmlFor="firstName" className="form-label">
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="firstName"
+                    name="firstName"
+                    required
+                  />
+                </div>
+                <div className="col-auto">
+                  <label htmlFor="middleName" className="form-label">
+                    Middle Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="middleName"
+                    name="middleName"
+                    required
+                  />
+                </div>
+                <div className="col">
+                  <label htmlFor="lastName" className="form-label">
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="lastName"
+                    name="lastName"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="row mb-3">
+                <div className="col">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="col">
+                  <label htmlFor="phoneNumber" className="form-label">
+                    Phone Number
+                  </label>
+                  <input
+                    type="tel"
+                    className="form-control"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="row mb-3">
+                <div className="col">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div className="col">
+                  <label htmlFor="confirmPassword" className="form-label">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="mt-5 text-center">
+                <button type="submit" className="btn btn-primary">
+                  Create Account
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
