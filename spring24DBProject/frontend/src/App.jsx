6888e6908a2e671025c74662bf889c15afa8952e
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-03-12 15:11:30 -0500

    adding signUp page routing

--- spring24DBProject/frontend/src/App.jsx
+++ spring24DBProject/frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import EmployeeLanding from './pages/EmployeeLanding';
-import CustomerLanding from './pages/CustomerLanding';
-import SignIn from './pages/SignIn';
-import AccountSetup from './pages/AccountSetup';
+import Home from "./pages/Home";
+import EmployeeLanding from "./pages/EmployeeLanding";
+import CustomerLanding from "./pages/CustomerLanding";
+import SignIn from "./pages/SignIn";
+import signUp from "./pages/signUp";
+import AccountSetup from "./pages/AccountSetup";
 
-import { Navbar } from './components/Navbar';
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/employeeLanding" element={<EmployeeLanding />} />
           <Route path="/customerLanding" element={<CustomerLanding />} />
           <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
           <Route path="/accountSetup" element={<AccountSetup />} />
         </Routes>
       </div>
---
+++ spring24DBProject/frontend/src/pages/signUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import classes from "../components/UI/signUp.module.css";
+
+export default function signUp() {
+  return <h1>SignUp Page</h1>;
+}
