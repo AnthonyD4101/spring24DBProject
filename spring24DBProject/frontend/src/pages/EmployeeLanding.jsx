54addc6cec7e231c7a7be7ad9f0d862bd1ea521c
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-03-12 23:09:40 -0500

    completed EmployeeSignIn, which directs user to EmployeeLanding/Portal

--- spring24DBProject/frontend/src/pages/EmployeeLanding.jsx
+++ spring24DBProject/frontend/src/pages/EmployeeLanding.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function EmployeeLanding() {
-  return <div></div>;
+  return <h1>Employee Landing Page/Portal</h1>;
 }
--- spring24DBProject/frontend/src/pages/EmployeeSignIn.jsx
+++ spring24DBProject/frontend/src/pages/EmployeeSignIn.jsx
@@ -1,5 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function EmployeeSignIn() {
-  return <h1>Employee Sign In</h1>;
+  return (
+    <div>
+      <div className="row justify-content-center">
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="my-4 text-center" style={{ color: "#2F4858" }}>
+                Staff Portal Sign In
+              </h1>
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail" className="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+
+                <div className="mb-3 pb-3">
+                  <label htmlFor="exampleInputPassword" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword"
+                  />
+                </div>
+
+                <div className="mt-5 text-center">
+                  <Link to="/employeeLanding" className="btn btn-primary">
+                    Sign In
+                  </Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
