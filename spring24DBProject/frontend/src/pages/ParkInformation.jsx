2652b83f85a8fc37d31a9f3e0d5e348c6c181847
Jonathan Vuong <jonathanvuong4@yahoo.com>
2024-03-14 21:53:24 -0500

    i set up very basic stuff for parkinfo, additions will be made shortly. this is just to test it

--- spring24DBProject/frontend/src/pages/ParkInformation.jsx
+++ spring24DBProject/frontend/src/pages/ParkInformation.jsx
@@ -1,5 +1,53 @@
 import React from "react";
 
+import classes from "../components/UI/Home.module.css";
+
+//alerts for temporarily closure shows up first, then today's park hours, and so on
 export default function ParkInformation() {
-  return <h1>Park Information</h1>;
+  return (
+    <div className={classes.homepage}>
+      <header>
+        <h1>
+          Park Information
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#today">Theme Park Hours</a>
+            </li>
+            <li>
+              <a href="#events">Show Times</a>
+            </li>
+            <li>
+              <a href="#parking">Parking Information</a>
+            </li>
+            <li>
+              <a href="#closed">Retired Attractions</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <section id="today" className={classes.about}>
+          
+        </section>
+        <section id="events" className={classes.about}>
+          
+        </section>
+        <section id="parking" className={classes.about}>
+          
+        </section>
+        <section id="closed" className={classes.about}>
+          
+        </section>
+
+      </main>
+
+
+
+    </div>
+  
+  
+  );
 }
